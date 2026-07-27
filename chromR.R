f58b952486f77536f5d8256baa09a09dfe6deb6e
mathiashole <mangino@fcien.edu.uy>
2026-07-26 22:43:57 -0300

    Update add feature points

--- chromR.R
+++ chromR.R
@@ -187,13 +187,11 @@ plot_chromosomes <- function(chrom_limits) {
 }
 
 add_feature_points <- function(p, features, colors) {
-  p +
-    geom_point(
-      data = features,
-      aes(x = mid_position, y = seqid, color = category),
-      size = 1.4
-    ) +
-    scale_color_manual(values = colors)
+  p + geom_point(
+    data = features,
+    aes(x = mid_position, y = seqid, color = category),
+    size = 1.4
+  )
 }
 
 add_feature_segments <- function(p, features, colors) {
